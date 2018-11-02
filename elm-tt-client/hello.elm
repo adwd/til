a39cdceb46ded6ab120f2e13c51b87cbd1bcd05b
adwd <ecdt.mn@gmail.com>
2018-11-02 15:12:08 +0900

    simple TEA

--- elm-tt-client/hello.elm
+++ elm-tt-client/hello.elm
@@ -1,7 +1,59 @@
 module Main exposing (main)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
+main : Program Never Model Msg
 main =
-    Html.text "hello"
+    Html.beginnerProgram
+        { model = { message = "" }
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { message : String }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = SayHello String
+    | Shutup
+    | SayBye
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SayHello greeted ->
+            { model | message = "hello" ++ greeted }
+
+        Shutup ->
+            { model | message = "" }
+
+        SayBye ->
+            { model | message = "bye" }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick (SayHello "world tree") ] [ text "hello" ]
+        , button [ onClick Shutup ] [ text "shutup" ]
+        , button [ onClick SayBye ] [ text "bye" ]
+        , span [ class "blue" ] [ text model.message ]
+        ]
