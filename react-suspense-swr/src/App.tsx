e416df74ab5d42dfebb64a3f874479b5be8160c3
adwd <ecdt.mn@gmail.com>
2019-10-29 21:42:50 +0900

    use GraphQL

--- react-suspense-swr/src/App.tsx
+++ react-suspense-swr/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import useSWR from '@zeit/swr';
+import { request } from 'graphql-request';
 
 const App = () => {
   return (
@@ -9,10 +10,23 @@ const App = () => {
   );
 };
 
+const API = 'https://api.spacex.land/graphql/';
+
 const Todos = () => {
   const { data, error } = useSWR(
-    'https://jsonplaceholder.typicode.com/todos',
-    (req: any) => fetch(req).then(res => res.json()),
+    `{
+      launchesPast(limit: 10) {
+        mission_name
+        launch_date_local
+        launch_site {
+          site_name_long
+        }
+        rocket {
+          rocket_name
+        }
+      }
+    }`,
+    (query: any) => request(API, query),
   );
 
   if (error) {
@@ -28,9 +42,17 @@ const Todos = () => {
   if (!data) {
     return <div>loading...</div>;
   }
-
+  console.log(data);
   return (
     <div>
+      {data.launchesPast.map((l: any) => (
+        <div key={l.mission_name}>
+          <h3>{l.mission_name}</h3>
+          <span>{l.launch_date_local}</span>
+          <p>{l.launch_site.site_name_long}</p>
+          <p>Rocket: {l.rocket.rocket_name}</p>
+        </div>
+      ))}
       <pre>
         <code>{JSON.stringify(data, null, 2)}</code>
       </pre>
