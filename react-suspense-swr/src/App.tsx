47285780a602abf8fae0950ecb6c565fd6fb7866
adwd <ecdt.mn@gmail.com>
2019-10-30 18:58:34 +0900

    There was no need to separate components that use Suspense from those that access resources

--- react-suspense-swr/src/App.tsx
+++ react-suspense-swr/src/App.tsx
@@ -2,31 +2,23 @@ import React, { Suspense } from 'react';
 import useSWR from '@zeit/swr';
 import { request } from 'graphql-request';
 
-const App = () => {
-  return (
-    <Suspense fallback={<div>loading...</div>}>
-      <Todos />
-    </Suspense>
-  );
-};
-
 const API = 'https://api.spacex.land/graphql/';
+const GET_LAUNCHES = `{
+  launchesPast(limit: 10) {
+    mission_name
+    launch_date_local
+    launch_site {
+      site_name_long
+    }
+    rocket {
+      rocket_name
+    }
+  }
+}`;
 
-const Todos = () => {
-  const { data, error } = useSWR(
-    `{
-      launchesPast(limit: 10) {
-        mission_name
-        launch_date_local
-        launch_site {
-          site_name_long
-        }
-        rocket {
-          rocket_name
-        }
-      }
-    }`,
-    (query: any) => request(API, query),
+const App = () => {
+  const { data, error } = useSWR(GET_LAUNCHES, (query: any) =>
+    request(API, query),
   );
 
   if (error) {
@@ -38,25 +30,25 @@ const Todos = () => {
       </div>
     );
   }
-
   if (!data) {
     return <div>loading...</div>;
   }
-  console.log(data);
   return (
-    <div>
-      {data.launchesPast.map((l: any) => (
-        <div key={l.mission_name}>
-          <h3>{l.mission_name}</h3>
-          <span>{l.launch_date_local}</span>
-          <p>{l.launch_site.site_name_long}</p>
-          <p>Rocket: {l.rocket.rocket_name}</p>
-        </div>
-      ))}
-      <pre>
-        <code>{JSON.stringify(data, null, 2)}</code>
-      </pre>
-    </div>
+    <Suspense fallback={<div>loading...</div>}>
+      <div>
+        {data.launchesPast.map((l: any) => (
+          <div key={l.mission_name}>
+            <h3>{l.mission_name}</h3>
+            <span>{l.launch_date_local}</span>
+            <p>{l.launch_site.site_name_long}</p>
+            <p>Rocket: {l.rocket.rocket_name}</p>
+          </div>
+        ))}
+        <pre>
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      </div>
+    </Suspense>
   );
 };
 
