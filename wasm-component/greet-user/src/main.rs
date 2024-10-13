c4d3b04605779aa9a7ca010bf567812a7300457d
adwd <adwd@users.noreply.github.com>
2024-10-13 22:16:22 +0900

    use bindgen

--- wasm-component/greet-user/src/main.rs
+++ wasm-component/greet-user/src/main.rs
@@ -1,8 +1,13 @@
 use clap::Parser;
 use wasmtime::{
-    component::{Component, Linker, TypedFunc},
+    component::{bindgen, Component, Linker},
     Engine, Store,
 };
+
+bindgen!({
+    world: "greetable-provider",
+    path: "../greet/wit"
+});
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -14,34 +19,16 @@ fn start(args: Args) -> anyhow::Result<()> {
     let component = Component::from_file(&engine, &args.wasm_file)?;
     let linker = Linker::new(&engine);
     let mut store = Store::new(&engine, ());
-    let instance = linker.instantiate(&mut store, &component)?;
 
-    let greetable_index = instance
-        .get_export(&mut store, None, "adwd:greet/greetable")
-        .unwrap();
-    let greet_index = instance
-        .get_export(&mut store, Some(&greetable_index), "greet")
-        .unwrap();
-    let name_index = instance
-        .get_export(&mut store, Some(&greetable_index), "name")
-        .unwrap();
-    let greet: TypedFunc<(String,), (String,)> =
-        instance.get_typed_func(&mut store, greet_index).unwrap();
-    let name: TypedFunc<(), (String,)> = instance.get_typed_func(&mut store, name_index).unwrap();
+    let provider = GreetableProvider::instantiate(&mut store, &component, &linker)?;
+    let greetable = provider.adwd_greet_greetable();
 
-    let argument = "world!".to_string();
-    let (return_value,) = greet.call(&mut store, (argument,))?;
-    greet.post_return(&mut store)?;
+    let message = greetable.call_greet(&mut store, "world")?;
+    println!("{}", message);
 
-    println!("{return_value}");
-
-    let (return_value,) = name.call(&mut store, ())?;
-    name.post_return(&mut store)?;
-
-    let (return_value,) = greet.call(&mut store, (return_value,))?;
-    greet.post_return(&mut store)?;
-
-    println!("{return_value}");
+    let name = greetable.call_name(&mut store)?;
+    let message = greetable.call_greet(&mut store, &name)?;
+    println!("{}", message);
 
     Ok(())
 }
